ba3953f6ef1cd24d276333b843331fab54b37d7f
Yurii Medvid <medvedimedved16@gmail.com>
2025-11-09 16:18:01 +0100

    correction of comments

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class CarWashStation:
     def __init__(
             self,
             distance_from_city_center: float,
-            clean_power: float,
+            clean_power: int,
             average_rating: float,
             count_of_ratings: int
     ) -> None:
@@ -32,7 +32,7 @@ class CarWashStation:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         cost_of_washing = ((
